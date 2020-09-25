682a857f5120db514591e3558c50b4627d980854
AlejandroMonge <kmonge_rey@hotmail.com>
2020-09-25 17:36:44 -0500

    Circulo Yin y Yang

--- Proyecto.py
+++ Proyecto.py
@@ -1,3 +1,18 @@
 print("Este sera un proyecto que dibujara un circulo yinyang")
 print("Kevin Alejandro Monge Reyes / 212556098")
 
+from turtle import *
+
+circle(120,360/2)
+
+begin_fill()
+circle(120, 360/2)
+end_fill()
+
+pu(); lt(90); fd(60)
+dot(120,"white"); dot(35,"black")
+fd(120)
+dot(120, "black"); dot(35, "white")
+
+ht(); done()
+
